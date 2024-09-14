8bc9cf0d472f89ac12a0af92935549a2656a68dc
ela <imela2811@gmail.com>
2024-09-14 15:23:05 -0600

    Start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,46 +5,36 @@ import {Button} from "flowbite-react";
 export default function Home() {
   const router = useRouter();
 
-  const handleDashboard1Click = () => {
-    router.push("/dashboard1");
+  const handleLoginClick = () => {
+    router.push("/login");
   };
-  const handleDashboard2Click = () => {
-    router.push("/dashboard2");
+  const handleSingingClick = () => {
+    router.push("/singin");
   };
-  const handleDashboard3Click = () => {
-    router.push("/dashboard3");
-  };
+
 
   return (
     <>
       <div className="bg-[url('/Background.svg')] bg-cover bg-center h-screen">
         <main className="flex min-h-screen flex-col items-center justify-between p-24 font-Oswald">
 
-        <h1 className="text-8xl font- font-title">This is vank</h1>
+        <h1 className="text-8xl font- font-title">This is FutureFunds</h1>
         <h2 className="text-4xl font-title">A place where u can learn</h2>
 
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={handleDashboard1Click}
+            onClick={handleLoginClick}
           >
-            Go to Dashboard1
+            Login
           </button>
 
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={handleDashboard2Click}
+            onClick={handleSingingClick}
           >
-            Go to Dashboard2
+            Sing in
           </button>
 
-          <button
-            className="bg-opacity-85 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
-            onClick={handleDashboard2Click}
-          >
-            Go to Dashboard3
-          </button>
-
-          
               <p className="text-2xl text-center font-title">
                   
               </p>  
